b95a9cfe854ba87d43cec490bef7cac5896161e0
sebewe <7thvex@gmail.com>
2023-07-21 18:22:40 +0000

    simple flask

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask import jsonify
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://flaskuser:STARTER@localhost/socialmusic_starter_db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+@app.route('/')
+def home():
+    return "Hello, World!"
+
+class User(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(50), nullable=False)
+
+@app.route('/add-user/<name>')
+def add_user(name):
+    new_user = User(name=name)
+    db.session.add(new_user)
+    db.session.commit()
+    return f"User {name} added successfully!"
+
+@app.route('/users')
+def users():
+	users = User.query.all()
+	return jsonify([user.name for user in users])
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
