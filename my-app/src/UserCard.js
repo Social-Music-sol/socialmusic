4e600d23a13921f0995ee2c77b479ca08c149ed0
frogdog1 <mishkakharlov1@gmail.com>
2023-07-21 16:43:59 -0700

    imageupload

--- my-app/src/UserCard.js
+++ my-app/src/UserCard.js
@@ -1,12 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './UserCard.css'; // Import CSS
 
 const UserCard = ({ user }) => {
-  return (
-    <div className="user-card">
-      <p>{user}</p>
-    </div>
-  );
+    const [image, setImage] = useState(null);
+
+    const handleImageChange = e => {
+        const file = e.target.files[0];
+        const reader = new FileReader();
+
+        reader.onloadend = () => {
+            setImage(reader.result);
+        }
+
+        if (file) {
+            reader.readAsDataURL(file);
+        } else {
+            setImage(null);
+        }
+    }
+
+    return (
+        <div className="user-card">
+            <p>{user}</p>
+            <input type="file" onChange={handleImageChange} accept="image/*" />
+            {image && <img src={image} alt="Profile" />}
+        </div>
+    );
 };
 
 export default UserCard;
