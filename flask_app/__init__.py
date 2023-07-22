a0b875ca20d4548743fa0a564dde9a1a107c64eb
sebewe <fireon06@gmail.com>
2023-07-22 09:43:24 -0700

    JWT token implementation

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -1,14 +1,18 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
+from dotenv import load_dotenv
+import os
 # Create the SQLAlchemy object
 db = SQLAlchemy()
 
 def create_app():
     # Create the Flask application
+    load_dotenv()
+
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://flaskuser:STARTER@localhost/socialmusic_starter_db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SECRET_KEY'] = os.getenv('FLASK_SECRET_KEY')
 
     # Now that we have the 'app' object, we can use it to initialize 'db'
     db.init_app(app)
