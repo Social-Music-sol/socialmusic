5d45b8d3813b286e7433a182ca2115e9fe7f973b
sebewe <fireon06@gmail.com>
2023-07-22 10:17:25 -0700

    authorization

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -14,6 +14,7 @@ def create_app():
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['SECRET_KEY'] = os.getenv('FLASK_SECRET_KEY')
     app.config["JWT_TOKEN_LOCATION"] = ["headers"]
+    app.config['JWT_HEADER_NAME'] = 'Authorization'
 
     # Now that we have the 'app' object, we can use it to initialize 'db'
     db.init_app(app)
