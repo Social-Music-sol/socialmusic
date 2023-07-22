4ee53322869348d4c887c87d1988d0f5794fe485
KeatonShawhan <shawhan.keaton@gmail.com>
2023-07-22 10:04:14 -0700

    putting user login into json

--- my-app/src/RegisterPage.js
+++ my-app/src/RegisterPage.js
@@ -1,32 +1,43 @@
 import React, { useState } from 'react';
 
-const RegisterPage = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [email, setEmail] = useState('');
+function RegisterPage() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log('Register with', { username, password, email });
-    // here you can call your API to register the user
+
+    const response = await fetch('/api/endpoint', { // replace '/api/endpoint' with your real API endpoint
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        username,
+        password,
+        email
+      })
+    });
+
+    if (response.ok) {
+      setUsername("");
+      setPassword("");
+      setEmail("");
+    } else {
+      // handle the error
+    }
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Username:
-        <input type="text" value={username} onChange={e => setUsername(e.target.value)} required />
-      </label>
-      <label>
-        Password:
-        <input type="password" value={password} onChange={e => setPassword(e.target.value)} required />
-      </label>
-      <label>
-        Email:
-        <input type="email" value={email} onChange={e => setEmail(e.target.value)} required />
-      </label>
-      <button type="submit">Register</button>
-    </form>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} required />
+        <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+        <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+        <button type="submit">Register</button>
+      </form>
+    </div>
   );
 }
 
