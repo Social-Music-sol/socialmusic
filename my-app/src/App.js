4892715d33f729b0b3a2e050a4426399de28de4c
frogdog1 <mishkakharlov1@gmail.com>
2023-07-21 18:28:26 -0700

    123

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,10 +2,7 @@ import React, { useState, useEffect } from 'react';
 import UserList from './UserList';
 import AddUserForm from './AddUserForm';
 import DeleteUserForm from './DeleteUserForm';
-import homePage from './homePage'; // Adjust the import path according to your project structure
-//import ProfilePage from './ProfilePage'; // Create and import your ProfilePage as well
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
-import ProfilePage from './ProfilePage';  // import the component for the profile page
+
 function App() {
   const [users, setUsers] = useState([]);
 
@@ -35,34 +32,12 @@ function App() {
   };
 
   return (
-    <Router>
-      <Switch>
-        <Route path="/profile">
-          <ProfilePage />  // This is the profile page component
-        </Route>
-        <Route path="/">
-          <div>
-            <h1>User List</h1>
-            {users.map((user, index) => (
-              <div key={index}>
-                <p>{user.name}</p>  {/* This assumes that user objects have a 'name' property */}
-                <button onClick={() => handleDeleteUser(user.name)}>Delete</button>
-              </div>
-            ))}
-            <form onSubmit={(event) => {
-              event.preventDefault();
-              const name = event.target.elements.name.value;
-              handleAddUser(name);
-              event.target.reset();
-            }}>
-              <input name="name" placeholder="New user name" required />
-              <button type="submit">Add User</button>
-            </form>
-            <Link to="/profile">Go to profile</Link>
-          </div>
-        </Route>
-      </Switch>
-    </Router>
+    <div className="App">
+      <h1>JamJar</h1>
+      <AddUserForm onUserAdd={handleAddUser} />
+      <DeleteUserForm onUserDelete={handleDeleteUser} />
+      <UserList users={users} />
+    </div>
   );
 }
 
