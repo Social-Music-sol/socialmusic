ddf20ce7374ba7233fa6af270f5e26919475daad
KeatonShawhan <shawhan.keaton@gmail.com>
2023-07-23 16:59:13 -0700

    fixing post

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,65 +6,70 @@ import PostForm from './PostForm';
 import UserPost from './UserPost';
 
 
-// inside App component
-const handlePost = async (e) => {
-  e.preventDefault();
-  
-  const response = await fetch('http://52.38.156.74:5000/post', { 
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    credentials: 'include', // to ensure that cookies are included in the request
-    body: JSON.stringify({
-      content,
-      image_url,
-      song_url
-    })
-  });
-
-  const data = await response.json();
-
-  if (data.message) {
-    alert(data.message);
-    setContent('');
-    setImageUrl('');
-    setSongUrl('');
-  }
-};
-
-/*
-// inside App return
-{posts.map(post => {
-  const user = user.find(user => user.id === post.userId);
-  return <UserPost key={post.id} post={post} user={user} />
-})}
-
-// inside App component
-const [posts, setPosts] = useState([]);
-
-const fetchUsersAndPosts = () => {
-  Promise.all([
-    fetch('/user/user_id'),  // replace with your real API endpoint
-    fetch('/posts')   // replace with your real API endpoint
-  ])
-  .then(([usersRes, postsRes]) => Promise.all([usersRes.json(), postsRes.json()]))
-  .then(([usersData, postsData]) => {
-    setUsers(usersData);
-    setPosts(postsData);
-  })
-  .catch(error => console.error('Error:', error));
-};
-
-// call fetchUsersAndPosts in useEffect
-useEffect(fetchUsersAndPosts, []);
-
-*/
-// inside App return
-<PostForm onPost={handlePost} />
 
 
 function App() {
+  const [content, setContent] = useState('');
+  const [imageUrl, setImageUrl] = useState('');
+  const [songUrl, setSongUrl] = useState('');
+
+  // inside App component
+  const handlePost = async (e) => {
+    e.preventDefault();
+    
+    const response = await fetch('http://52.38.156.74:5000/post', { 
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      credentials: 'include', // to ensure that cookies are included in the request
+      body: JSON.stringify({
+        content,
+        imageUrl,
+        songUrl
+      })
+    });
+
+    const data = await response.json();
+
+    if (data.message) {
+      alert(data.message);
+      setContent('');
+      setImageUrl('');
+      setSongUrl('');
+    }
+  };
+
+  /*
+  // inside App return
+  {posts.map(post => {
+    const user = user.find(user => user.id === post.userId);
+    return <UserPost key={post.id} post={post} user={user} />
+  })}
+
+  // inside App component
+  const [posts, setPosts] = useState([]);
+
+  const fetchUsersAndPosts = () => {
+    Promise.all([
+      fetch('/user/user_id'),  // replace with your real API endpoint
+      fetch('/posts')   // replace with your real API endpoint
+    ])
+    .then(([usersRes, postsRes]) => Promise.all([usersRes.json(), postsRes.json()]))
+    .then(([usersData, postsData]) => {
+      setUsers(usersData);
+      setPosts(postsData);
+    })
+    .catch(error => console.error('Error:', error));
+  };
+
+  // call fetchUsersAndPosts in useEffect
+  useEffect(fetchUsersAndPosts, []);
+
+  */
+  // inside App return
+  <PostForm onPost={handlePost} />
+
 
   return (
     <Router>
@@ -90,3 +95,4 @@ function App() {
 }
 
 export default App;
+
