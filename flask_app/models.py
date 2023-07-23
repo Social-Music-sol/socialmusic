408bb14586b1c3e75f60e8bcb676e888ef5b0741
sebewe <fireon06@gmail.com>
2023-07-23 11:52:33 -0700

    Update models.py

--- flask_app/models.py
+++ flask_app/models.py
@@ -16,11 +16,12 @@ else:
 class User(db.Model):
     __tablename__ = 'users'
 
-    id = db.Column(db.String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
+    id = db.Column(db.UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     username = db.Column(db.String(64), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128), nullable=False)
-    salt = db.Column(db.String(128), nullable=False)
+    password_hash = db.Column(db.String(64), nullable=False)
+    salt = db.Column(db.String(64), nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
     def set_password(self, password):
         self.salt = uuid.uuid4().hex
@@ -29,6 +30,24 @@ class User(db.Model):
     def check_password(self, password):
         return check_password_hash(self.password_hash, self.salt + password)
     
+class Post(db.Model):
+    __tablename__ = 'posts'
+
+    id = db.Column(db.UUID(as_uuid=True), primary_key=True, unique=True, default=uuid.uuid4)
+    user_id = db.Column(db.UUID(as_uuid=True), db.ForeignKey('users.id'), nullable=False)
+    parent_id = db.Column(db.UUID(as_uuid=True), db.ForeignKey('posts.id'), nullable=True)
+    content = db.Column(db.Integer(300), nullable=False)
+    replies = db.relationship('Post', backref=db.backref('parent', remote_side=['id']), lazy='dynamic')
+    likes = db.relationship('Like', backref='post', lazy='dynamic')
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+class Like(db.Model):
+    __tablename__ = 'likes'
+
+    id = db.Column(db.UUID(as_uuid=True), primary_key=True, unique=True, default=uuid.uuid4) 
+    user_id = db.Column(db.UUID(as_uuid=True), db.ForeignKey('users.id'), nullable=False)
+    post_id = db.Columns(db.UUID(as_uuid=True), db.ForeignKey('posts.id'), nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
 
 if __name__ == "__main__":  
