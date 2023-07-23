f1f1c21710bf119a61c9cc3197108b363de4678c
KeatonShawhan <shawhan.keaton@gmail.com>
2023-07-23 16:11:00 -0700

    s

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link, useLocation } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import RegisterPage from './RegisterPage';
 import LoginPage from './LoginPage';
 
