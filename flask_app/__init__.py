f9b995e845d61b78267ea58583ad06203e75610b
sebewe <fireon06@gmail.com>
2023-07-22 17:05:41 -0700

    Update __init__.py

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -26,7 +26,7 @@ def create_app():
 
 
     # Then we import and register blueprints
-    import routes as routes_blueprint
+    import flask_app.routes as routes_blueprint
     app.register_blueprint(routes_blueprint)
 
     return app
