4ce01653c1cf5efc5cb7de2b70e5f77389d1e539
KeatonShawhan <shawhan.keaton@gmail.com>
2023-07-24 14:01:09 -0700

    using global domain

--- my-app/src/LoginPage.js
+++ my-app/src/LoginPage.js
@@ -7,7 +7,7 @@ function LoginPage() {
   const handleLogin = async (e) => {
     e.preventDefault();
   
-    const response = await fetch('https://findingnasa.xyz/api/login', {
+    const response = await fetch(process.env.API_DOMAIN + "/login", {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
