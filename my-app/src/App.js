3612d753525f7fe1d9771f4402a196d5f1ccac37
KeatonShawhan <shawhan.keaton@gmail.com>
2023-07-24 10:50:44 -0700

    trying to clean up handlePost

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,56 +1,32 @@
-import React, { useState } from 'react';
+// App.js
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import RegisterPage from './RegisterPage';
 import LoginPage from './LoginPage';
 import PostForm from './PostForm';
 
-
 function App() {
-  const handlePost = async (songLink, pictureUrl, caption) => {
-
-    const response = await fetch('https://findingnasa.xyz/api/post', { 
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      credentials: 'include',
-      body: JSON.stringify({
-        content: caption,
-        image_url: pictureUrl,
-        song_url: songLink
-      })
-    });
-  
-    const data = await response.json();
-  
-    if (data.message) {
-      alert(data.message);
-    }
-  };
-
-  <PostForm onPost={handlePost} />
   return (
     <Router>
-    <div className="App">
-      <Routes>
-        <Route path="/" element={
-          <div>
-            <h1>JamJar</h1>
-            <Link to="/register">Register</Link>
-            <br />
-            <Link to="/login">Login</Link>
-            <br />
-            <Link to="/post">Create a Post</Link>
-          </div>
-        } />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/post" element={<PostForm onPost={handlePost} />} /> 
-      </Routes>
-    </div>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={
+            <div>
+              <h1>JamJar</h1>
+              <Link to="/register">Register</Link>
+              <br />
+              <Link to="/login">Login</Link>
+              <br />
+              <Link to="/post">Create a Post</Link>
+            </div>
+          } />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/post" element={<PostForm />} /> 
+        </Routes>
+      </div>
     </Router>
   );
 }
 
 export default App;
-
--- my-app/src/PostForm.js
+++ my-app/src/PostForm.js
@@ -1,20 +1,39 @@
 // PostForm.js
 import React, { useState } from 'react';
 
-const PostForm = ({ onPost }) => {
+const PostForm = () => {
   const [songLink, setSongLink] = useState('');
   const [pictureUrl, setPictureUrl] = useState('');
   const [caption, setCaption] = useState('');
 
+  const handlePost = async (songLink, pictureUrl, caption) => {
+    const response = await fetch('https://findingnasa.xyz/api/post', { 
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      credentials: 'include',
+      body: JSON.stringify({
+        content: caption,
+        image_url: pictureUrl,
+        song_url: songLink
+      })
+    });
+
+    const data = await response.json();
+
+    if (data.message) {
+      alert(data.message);
+    }
+  };
+
   const handleSubmit = (event) => {
     event.preventDefault();
-    // We pass the songLink, pictureUrl, and caption as separate arguments, not as an object
-    onPost(songLink, pictureUrl, caption);
+    handlePost(songLink, pictureUrl, caption);
     setSongLink('');
     setPictureUrl('');
     setCaption('');
   }
-  
 
   return (
     <form onSubmit={handleSubmit}>
