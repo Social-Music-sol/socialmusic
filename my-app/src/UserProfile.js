6a81d549431361b635b9b1af885c3096a6dfe733
KeatonShawhan <shawhan.keaton@gmail.com>
2023-07-25 12:17:57 -0700

    trying to get posts on each user profile

--- my-app/src/UserProfile.js
+++ my-app/src/UserProfile.js
@@ -1,17 +1,43 @@
 // UserProfile.js
-import React from 'react';
-import { Link, useParams } from "react-router-dom";
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
-function UserProfile() {
-  // We use the `useParams` hook here to get the dynamic parts of the URL.
-  let { username } = useParams();
+export default function UserProfile() {
+  const { username } = useParams();
+  const [posts, setPosts] = useState([]);
+
+  const getUserPosts = async () => {
+    const userId = localStorage.getItem('userId');
+
+    if (!userId) return;
+
+    const response = await fetch(`${process.env.REACT_APP_API_DOMAIN}/get-posts/${userId}`);
+
+    if (response.ok) {
+      const postsData = await response.json();
+      setPosts(postsData);
+    }
+  };
+
+  useEffect(() => {
+    getUserPosts();
+  }, []);
 
   return (
     <div>
-      <h2>User Profile: {username}</h2>
+      <h1>{username}'s Profile</h1>
       <Link to="/">Go to Homepage</Link>
+      <h2>Posts:</h2>
+      {posts.map((post, index) => (
+        <div key={index}>
+          <h3>Post {index + 1}</h3>
+          <p>{post.song_url}</p>
+          <p>{post.image_url}</p>
+          <p>{post.content}</p>
+          <p>{post.created_at}</p>
+        </div>
+      ))}
     </div>
   );
 }
-
-export default UserProfile;
