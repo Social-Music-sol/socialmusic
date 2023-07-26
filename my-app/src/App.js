dd74b82c87fd2d51ff8881bade83573bde496897
KeatonShawhan <shawhan.keaton@gmail.com>
2023-07-26 11:06:52 -0700

    fixing imports

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,7 +6,6 @@ import LoginPage from './LoginPage';
 import PostForm from './PostPage';
 import UserProfile from './ProfilePage'; 
 import { getLoggedInUser, handleLogout, handleLike } from './utils';
-import { getLoggedInUser, handleLogout } from './utils';
 import './App.css'; // Import your CSS file
 import textlogo from './images/textlogo.png'
 import pfp from './images/circle.png';  // import the profile icon image
