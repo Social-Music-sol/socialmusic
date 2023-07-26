0db8eeae0629e2acc0b7f543decd64b38e615483
KeatonShawhan <shawhan.keaton@gmail.com>
2023-07-25 19:40:34 -0700

    trying to clean up app.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,56 +5,8 @@ import RegisterPage from './RegisterPage';
 import LoginPage from './LoginPage';
 import PostForm from './PostPage';
 import UserProfile from './ProfilePage'; 
-import { getLoggedInUser, handleLogout } from './utils';
+import HomePage from './homepage';
 
-
-function HomePage() {
-  const username = getLoggedInUser();
-  const [posts, setPosts] = useState([]);
-
-  useEffect(() => {
-    const getRecentPosts = async () => {
-      const response = await fetch(`${process.env.REACT_APP_API_DOMAIN}/recent-feed`);
-
-      if (response.ok) {
-        const postsData = await response.json();
-        setPosts(postsData);
-      }
-    };
-
-    getRecentPosts();
-  }, []);
-
-  return (
-    <div>
-      <h1>JamJar</h1>
-      {!username && <Link to="/register">Register</Link>}
-      <br />
-      {!username && <Link to="/login">Login</Link>}
-      <br />
-      {username && <Link to="/post">Create a Post</Link>}
-      <br />
-      {username && <Link to={`/users/${username}`}>Go to Profile</Link>}
-      <br />
-      {username && <button onClick={handleLogout}>Logout</button>}
-      <h2>Recent Posts:</h2>
-      {posts.map((post, index) => (
-        <div key={index}>
-          <Link to={`/users/${post.username}`}>
-            <h3>{post.username}</h3>
-          </Link>
-          <p>{post.content}</p>
-          <p>{post.image_url}</p>
-          <p>{post.created_at}</p>
-          <div style={{left: 0, width: 900, height: 180, position: 'relative'}} dangerouslySetInnerHTML={{
-            __html: `<iframe src=${post.song_embed_url} style="top: 0; left: 0; width: 100%; height: 100%; position: absolute; border: 0;" allowfullscreen allow="clipboard-write; encrypted-media; fullscreen; picture-in-picture; autoplay;"></iframe>`
-          }}>
-          </div>
-        </div>
-      ))}
-    </div>
-  );
-}
 
 function App() {
   return (
