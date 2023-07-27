6aacd1ac2594f2e4e3b3737bb29ea85c226e6703
KeatonShawhan <shawhan.keaton@gmail.com>
2023-07-27 10:16:45 -0700

    deleting some imports

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,15 +1,10 @@
 // App.js
 import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHeart } from '@fortawesome/free-solid-svg-icons';
 import RegisterPage from './RegisterPage';
 import LoginPage from './LoginPage';
 import PostForm from './PostPage';
 import UserProfile from './ProfilePage'; 
-import { getLoggedInUser, handleLogout, handleLike } from './utils';
-import textlogo from './images/textlogo.png'
-import pfp from './images/circle.png';  // import the profile icon image
 import FrontPage from './FrontPage'; // update the path according to your directory structure
 
 function App() {
