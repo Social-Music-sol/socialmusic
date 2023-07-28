48e570ee9d1ee3c90b02b9d7926ad4a5aba7dcb9
KeatonShawhan <shawhan.keaton@gmail.com>
2023-07-28 09:38:54 -0700

    deleting comment box on no entry

--- my-app/src/FrontPage.js
+++ my-app/src/FrontPage.js
@@ -54,37 +54,39 @@ function HomePage() {
         <br />
         <div className="posts-container">
         {posts.map((post, index) => (
-  <div key={index} className="post-box">
-    <div className="post-header">
-      <Link to={`/users/${post.username}`} className="profile-link">
-        <FontAwesomeIcon icon={faCircle} className="profile-icon" />
-      </Link>
-      <h3>{post.username}</h3>
-    </div>
-    <div className="post-content">
-    <div className="post-embed">
-      <div className="embed-container" dangerouslySetInnerHTML={{
-        __html: `<iframe src=${post.song_embed_url} class="spotify-embed" allowfullscreen allow="clipboard-write; encrypted-media; fullscreen; picture-in-picture; autoplay;"></iframe>`
-      }}>
-      </div>
-    </div>
-      <div className="post-text-container">
-        <div className="post-text">
-          <p>{post.content}</p>
-        </div>
-      </div>
-    </div>
-    <div className="like-container">
-      <FontAwesomeIcon 
-        icon={post.liked_by_requester ? faHeart : faHeart} 
-        className="like-button" 
-        style={{ color: post.liked_by_requester ? 'red' : 'pink' }}
-        onClick={() => handleLike(post.id, posts, setPosts)}
-      />
-      <p>{post.like_count}</p>
-    </div>
-  </div>
-))}
+          <div key={index} className="post-box">
+            <div className="post-header">
+              <Link to={`/users/${post.username}`} className="profile-link">
+                <FontAwesomeIcon icon={faCircle} className="profile-icon" />
+              </Link>
+              <h3>{post.username}</h3>
+            </div>
+            <div className="post-content">
+            <div className="post-embed">
+              <div className="embed-container" dangerouslySetInnerHTML={{
+                __html: `<iframe src=${post.song_embed_url} class="spotify-embed" allowfullscreen allow="clipboard-write; encrypted-media; fullscreen; picture-in-picture; autoplay;"></iframe>`
+              }}>
+              </div>
+            </div>
+              {post.content && ( // Check if post.content is not empty
+                <div className="post-text-container">
+                  <div className="post-text">
+                    <p>{post.content}</p>
+                  </div>
+                </div>
+              )}
+            </div>
+            <div className="like-container">
+              <FontAwesomeIcon 
+                icon={post.liked_by_requester ? faHeart : faHeart} 
+                className="like-button" 
+                style={{ color: post.liked_by_requester ? 'red' : 'pink' }}
+                onClick={() => handleLike(post.id, posts, setPosts)}
+              />
+              <p>{post.like_count}</p>
+            </div>
+          </div>
+        ))}
         </div>
       </div>
     );
