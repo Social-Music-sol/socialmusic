98c5fcd9ea8295dca78cdaed09138bd0241a0278
frogdog1 <mishkakharlov1@gmail.com>
2023-07-28 13:10:29 -0700

    Revert "comment container-homepage redo"
    This reverts commit 048b0e3e3672d4c2cd2bb79923d6dc70002664b8.

--- my-app/src/FrontPage.js
+++ my-app/src/FrontPage.js
@@ -75,20 +75,21 @@ function HomePage() {
   return (
     <div className="container">
       <div className="header">
-        <div className="header-left">
-          <img src={textlogo} alt="JamJar Text Logo" className="textlogo" />
-          {username && 
-            <Link to="/post" className="create-post-button">
-              <button className="post-button">+++</button>
-            </Link>
-          }
-        </div>
+            <div className="header-left">
+        <img src={textlogo} alt="JamJar Text Logo" className="textlogo" />
+        {username && 
+          <Link to="/post" className="create-post-button">
+            <button className="post-button">+++</button>
+          </Link>
+        }
+      </div>
         <div className="header-right">
           {username && 
             <div className="pfp-container">
               <Link to={`/users/${username}`} className="pfp-link">
                 <img src={userProfilePic || pfp} alt="Profile Icon" className="pfp" /> 
               </Link>
+              <button className="logout-button" onClick={handleLogout}>Log-out</button>
             </div>
           }
         </div>
@@ -121,23 +122,22 @@ function HomePage() {
                 </div>
               )}
               <div className="comments-section">
-                {post.replies.map((reply, index) => (
-                  <div key={index} className="comment-container">
-                    <div className="reply-box">
-                      <div className="reply-header">
-                        <Link to={`/users/${reply.username}`} className="profile-link">
-                          <img src={reply.poster_pfp_url} alt={`${reply.username}'s profile`} className="profile-icon" />
-                        </Link>
-                        <h3>{reply.username}</h3>
-                      </div>
-                      <p>{reply.content}</p>
-                    </div>
+              {post.replies.map((reply, index) => (
+                <div key={index} className="reply-box">
+                  <div className="reply-header">
+                    <Link to={`/users/${reply.username}`} className="profile-link">
+                      <img src={reply.poster_pfp_url} alt={`${reply.username}'s profile`} className="profile-icon" />
+                    </Link>
+                    <h3>{reply.username}</h3>
                   </div>
-                ))}
-                <form onSubmit={(e) => handleCommentSubmit(e, post.id)} className="comment-form">
-                  <input type="text" name="comment" placeholder="Add a comment..." />
-                  <button type="submit">Comment</button>
-                </form>
+                  <p>{reply.content}</p>
+                  {/* You can add more elements here as per your design */}
+                </div>
+              ))}
+              <form onSubmit={(e) => handleCommentSubmit(e, post.id)} className="comment-form">
+                <input type="text" name="comment" placeholder="Add a comment..." />
+                <button type="submit">Comment</button>
+              </form>
               </div>
             </div>
             <div className="like-container">
