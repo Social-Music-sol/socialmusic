45b5213bc2d09051918a66790424b0a94f3b9521
sebewe <fireon06@gmail.com>
2023-07-27 18:24:14 -0700

    huh?

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager
 from flask_cors import CORS
 from datetime import timedelta
+from werkzeug.utils import secure_filename
 import os
 # Create the SQLAlchemy object
 db = SQLAlchemy()
