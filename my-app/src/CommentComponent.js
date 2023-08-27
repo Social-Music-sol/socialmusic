7965b428d45f0327b7073492c5a9bb5eccecca84
sebewe <fireon06@gmail.com>
2023-08-27 15:26:28 -0700

    npm run build

--- my-app/src/CommentComponent.js
+++ my-app/src/CommentComponent.js
@@ -5,29 +5,30 @@ import { Link } from "react-router-dom";
 import { handleLike } from './utils';
 
 function CommentComponent(props) {
-  const {reply, index, comments, setComments} = props;
-
-  return (
-    <div key={index + 3} className="reply-box"> {/* added +3 to avoid potential key conflicts */}
-            <div className="reply-header">
-                <Link to={`/users/${reply.username}`} className="profile-link">
-                    <img src={reply.poster_pfp_url} alt={`${reply.username}'s profile`} className="profile-icon" />
-                </Link>
-                <h3>{reply.username}</h3>
-            </div>
-            {/* <div className="comment-like-container">
-                <FontAwesomeIcon 
-                icon={reply.liked_by_requester ? faHeart : faHeart} 
-                className="like-button" 
-                style={{ color: reply.liked_by_requester ? 'red' : 'pink' }}
-                onClick={() => handleLike(reply.id, comments, setComments)}
-                />
+    const {reply, index, comments, setComments} = props;
+  
+    return (
+      <div key={index + 3} className="reply-box">
+        <div className="reply-header">
+          <Link to={`/users/${reply.username}`} className="profile-link">
+            <img src={reply.poster_pfp_url} alt={`${reply.username}'s profile`} className="profile-icon" />
+          </Link>
+          <h3>{reply.username}</h3>
+  
+          <div className="comment-like-container">
+            <FontAwesomeIcon 
+              icon={reply.liked_by_requester ? faHeart : faHeart} 
+              className="like-button" 
+              style={{ color: reply.liked_by_requester ? 'red' : 'pink' }}
+              onClick={() => handleLike(reply.id, comments, setComments)}
+            />
             <p className="like-count">{reply.like_count}</p>
-            </div> */}
-            <p>{reply.content}</p>
-        
-    </div>
+          </div>
+        </div>
+        <p>{reply.content}</p>
+      </div>
     );
-}
+  }
+  
 
 export default CommentComponent;
